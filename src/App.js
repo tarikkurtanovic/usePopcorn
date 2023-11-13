cbdeeea14bf5550f402a55b86cce8c50ad8a01e8
tarikkurtanovic <tarik2371@gmail.com>
2023-11-13 12:38:09 +0100

    min fix

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok)
